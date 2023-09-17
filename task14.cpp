c1b5c30809833196aab2420e9289adcfe53273f2
Khadija Saeed <144422229+khadijasaeed683@users.noreply.github.com>
2023-09-17 14:24:49 +0500

    Add files via upload

---
+++ task14.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"   '._                   "<<endl;
+cout<<"   ''  ''           ---,   "<<endl;
+cout<<"    " . "!_...._/' .-'      "<<endl;
+cout<<"      |            /     '    "<<endl;
+cout<<"      / ()     () !    .' '-._   "<<endl;
+cout<<"     |)          ()!  /   _.'    "<<endl;
+cout<<"      |    -'-      ,; '. <      "<<endl;
+cout<<"      ;'--        ,;|   > |    "<<endl;
+cout<<"     / ,      / ,   |.-'.-'         "<<endl;
+cout<<"    (_/      (_/  ,;|.<'                     "<<endl;
+cout<<"       !     ,      ;-'               "<<endl;
+cout<<"        >    !     /                 "<<endl;
+cout<<"       (_,-' '> ..'                          "<<endl;
+cout<<"         (_,'                       "<<endl;
+}
