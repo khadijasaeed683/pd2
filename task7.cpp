945182f7ae3663e7a60167db0aed998453fd4784
Khadija Saeed <144422229+khadijasaeed683@users.noreply.github.com>
2023-09-17 15:21:48 +0500

    Add files via upload

---
+++ task7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+use namespace std;
+main()
+{
+system ( "color 79");
+cout<< "      .::---::.." << endl;
+cout<< "    .-----------.." << endl;
+cout<< "   .---------------." << endl;
+cout<< "  --------------:." << endl;
+cout<< " :.--------::." << endl;
+cout<< " --------------:." << endl;
+cout<< "  :-------------:.." << endl;
+cout<< "   .---------------:" << endl;
+cout<< "    .:--------:." << endl;
+}
